1346544712980d335e7658e204342edfa08c093c
yagizhankd <yagizhankerim@gmail.com>
2021-01-21 15:05:08 +0300

    spring boot things

---
+++ src/main/java/scooter_sharing/app/ScooterSharingApplication.java
@@ -0,0 +1,13 @@
+package scooter_sharing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooterSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScooterSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scooter_sharing/app/ScooterSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package scooter_sharing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooterSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
